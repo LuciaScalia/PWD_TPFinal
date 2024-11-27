39aabc1f57955483f7b60da29e95679cbe66d2e1
Nadia-Carrasco1 <nadia.carrasco@est.fi.uncoma.edu.ar>
2024-11-27 12:33:35 -0300

    cambio accion realizarcompras

--- Vista/Compra/accion/realizarCompra.php
+++ Vista/Compra/accion/realizarCompra.php
@@ -1,9 +1,8 @@
 <?php
 include_once '../../../configuracion.php';
-/*
+
 $datosJSON=file_get_contents('php://input');
-$datosCompra= json_decode($datosJSON, true);*/
-$datosCompra=  data_submitted();
+$datosCompra= json_decode($datosJSON, true);
 
 if(isset($datosCompra['productos'])){
     $productos=$datosCompra['productos'];
